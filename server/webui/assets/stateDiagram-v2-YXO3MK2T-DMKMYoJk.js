c523880423fd013c95bd6057f847f9db78e2bf80
Adam Chesney <ajchesney@gmail.com>
2025-05-09 12:57:41 +0100

    Release 2.1.0 with post edit validation/linting etc

--- server/webui/assets/stateDiagram-v2-YXO3MK2T-DMKMYoJk.js
+++
@@ -1,1 +0,0 @@
-import{s as t,S as a,b as r,a as s}from"./chunk-AEK57VVT-DKCo6o6I.js";import{_ as i}from"./index-CwzAko7O.js";import"./chunk-RZ5BOZE2-CC-ni-Xj.js";var _={parser:t,get db(){return new a(2)},renderer:r,styles:s,init:i((r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute}),"init")};export{_ as diagram};

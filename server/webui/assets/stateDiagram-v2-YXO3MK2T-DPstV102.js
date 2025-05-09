1402567fade2e71c5c412dddff8a3d7f59bd1d9f
Adam Chesney <ajchesney@gmail.com>
2025-05-09 16:44:49 +0100

    Release 2.1.1 with CMD+Enter new line and typescript validation bugfix

--- server/webui/assets/stateDiagram-v2-YXO3MK2T-DPstV102.js
+++
@@ -1,1 +0,0 @@
-import{s as t,S as a,b as r,a as s}from"./chunk-AEK57VVT-Bp615Ev3.js";import{_ as i}from"./index-D6TZ2Y2B.js";import"./chunk-RZ5BOZE2-DNbvwFWK.js";var _={parser:t,get db(){return new a(2)},renderer:r,styles:s,init:i((r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute}),"init")};export{_ as diagram};

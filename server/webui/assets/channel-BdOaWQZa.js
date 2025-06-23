b9871cbcc894392a7005aa7a5a5e1bd64c0054ba
Adam Chesney <ajchesney@gmail.com>
2025-06-23 10:42:52 +0100

    Release 2.9.3 - fixes broken UI

---
+++ server/webui/assets/channel-BdOaWQZa.js
@@ -0,0 +1,1 @@
+import{ao as r,ap as n}from"./index-qo37nOHw.js";const t=(a,o)=>r.lang.round(n.parse(a)[o]);export{t as c};
---
+++ server/webui/assets/clone-BZso_t2X.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-Xgg48N_M.js";var e=4;function a(a){return r(a,e)}export{a as c};

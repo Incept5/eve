e6eef743df5bc03b0ac165557106bc8e82183a61
Adam Chesney <ajchesney@gmail.com>
2025-08-13 15:21:19 +0100

    Release 2.11.0 with Open Router provider and full MCP re-architecture

---
+++ server/webui/assets/chunk-XZIHB7SX-DVWyIoIo.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index-jDBr2C2C.js";var t,e=(s(t=class{constructor(t){this.init=t,this.records=this.init()}reset(){this.records=this.init()}},"ImperativeState"),t);export{e as I};
---
+++ server/webui/assets/classDiagram-v2-COTLJTTW-D2A1YjRw.js
@@ -0,0 +1,1 @@
+import{c as r,C as s,a as e,s as t}from"./chunk-A2AXSNBT-BQ4_uL1T.js";import{_ as l}from"./index-jDBr2C2C.js";import"./chunk-RZ5BOZE2-DhYdgwsu.js";var m={parser:r,get db(){return new s},renderer:e,styles:t,init:l((r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute}),"init")};export{m as diagram};
